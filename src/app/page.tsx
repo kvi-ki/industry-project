f8ceeed0a7afdc2f428da9289c0f67a7d69ff773
Victoria Kovaleva <victoriak.dev@gmail.com>
2024-02-20 11:16:50 +0100

    chore: add dynamicHeader

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
-import Header from "./components/header/Header";
+import Header from './components/header/Header';
+
+import dynamic from 'next/dynamic';
+
+const DynamicHeader = dynamic(() => import('./components/header/Header'), {
+	ssr: false,
+});
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Header />
-    </main>
-  );
+	return (
+		<main className='flex min-h-screen flex-col items-center justify-between p-24'>
+			<DynamicHeader />
+		</main>
+	);
 }
